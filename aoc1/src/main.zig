53fc8190d2ae3a9dacd02987d18766f9afb60e52
David Rebbe <1187684+ic3man5@users.noreply.github.com>
2024-12-21 16:13:29 -0500

    aoc1p2 works

--- aoc1/src/main.zig
+++ aoc1/src/main.zig
@@ -1,5 +1,15 @@
 const std = @import("std");
 
+pub fn countOccurrences(comptime T: type, value: T, items: []T) i64 {
+    var count: i64 = 0;
+    for (items) |item| {
+        if (value == item) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
 pub fn main() !void {
     // Heap allocator
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -32,34 +42,13 @@ pub fn main() !void {
         try left.append(try std.fmt.parseInt(i64, left_number, 10));
         try right.append(try std.fmt.parseInt(i64, right_number, 10));
     }
-    // print the length of both
-    std.debug.print(
-        "{} {}\n",
-        .{ left.items.len, right.items.len },
-    );
-    // Check to make sure left and right are the same length
-    if (left.items.len != right.items.len) {
-        @panic("left and right items are not synced!");
-    }
     // Sort both left and right
     std.mem.sort(i64, left.items, {}, comptime std.sort.asc(i64));
     std.mem.sort(i64, right.items, {}, comptime std.sort.asc(i64));
 
-    var total_distance: u64 = 0;
-    for (left.items, right.items) |left_num, right_num| {
-        total_distance += @abs(left_num - right_num);
+    var total_similarity: i64 = 0;
+    for (left.items) |item| {
+        total_similarity += item * countOccurrences(i64, item, right.items);
     }
-    std.debug.print("Total Distance: {}", .{total_distance});
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "convert string to int" {
-    const value = try std.fmt.parseInt(u64, "145752", 10);
-    try std.testing.expect(value == 14575);
+    std.debug.print("Total Similarity: {}", .{total_similarity});
 }
